508957bdab49b6a077b66623d253693982ad0d16
ibrivasortiz92 <78561871+ibrivasortiz92@users.noreply.github.com>
2023-08-28 20:41:55 -0300

    Add static and media roots

--- core/settings.py
+++ core/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'public' / 'site' / 'static'
+MEDIA_URL = '/media'
+MEDIA_ROOT = BASE_DIR / 'public' / 'site' / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
